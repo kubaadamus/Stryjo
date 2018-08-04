7f7bc97ee77a4c49f85defce076ef09a39f4d7e1
kubaadamus <jakubadamus1991@gmail.com>
2018-08-04 21:27:55 +0200

    Wszystko działa bezbłędnie. Design SKONCZONY. Teraz wezme sie za mobile first i RWD

--- app/assets/scripts/cennik.js
+++ app/assets/scripts/cennik.js
@@ -9,47 +9,46 @@ var _6;
 var suma;
 var rabat = false;
 $('input[type=range]').bind('input', function (evt) {
-    var nazwa_docelowa = $(this).attr('id')+"box";
-    $("#"+nazwa_docelowa+"").attr('value',this.value);
+    var nazwa_docelowa = $(this).attr('id') + "box";
+    $("#" + nazwa_docelowa + "").attr('value', this.value);
 
 
 
 
-/*NALICZANIE KWOTY*/
+    /*NALICZANIE KWOTY*/
 
-dor = $("#_dorbox").attr('value');
-dz = $("#_dzbox").attr('value');
-_1 = $("#_1box").attr('value');
-_2 = $("#_2box").attr('value');
-_3 = $("#_3box").attr('value');
-_4 = $("#_4box").attr('value');
-_5 = $("#_5box").attr('value');
-_6 = $("#_6box").attr('value');
+    dor = $("#_dorbox").attr('value');
+    dz = $("#_dzbox").attr('value');
+    _1 = $("#_1box").attr('value');
+    _2 = $("#_2box").attr('value');
+    _3 = $("#_3box").attr('value');
+    _4 = $("#_4box").attr('value');
+    _5 = $("#_5box").attr('value');
+    _6 = $("#_6box").attr('value');
 
-console.log(dor);
-console.log(dz);
-console.log(_3);
-console.log(_4);
+    console.log(dor);
+    console.log(dz);
+    console.log(_3);
+    console.log(_4);
 
-suma = (dor*100+dz*50+_1*2+_3*1);
+    suma = (dor * 100 + dz * 50 + _1 * 2 + _3 * 1);
 
-if(dz>=1)
-{
-    rabat=true;
-    $("#rabatAlert").show();
+    if (dz >= 1 && dor>=2) {
+        rabat = true;
+        $("#rabatAlert").show();
 
-    suma = suma*0.9;
-}
-else{
-    rabat=false;
-    $("#rabatAlert").hide();
-}
+        $("#oszczedzasz").html((suma-suma*0.9).toFixed(2)+" PLN");
 
-suma = suma.toFixed(2);
+        suma = suma * 0.9;
+    } else {
+        rabat = false;
+        $("#rabatAlert").hide();
+    }
 
-suma += " PLN";
+    suma = suma.toFixed(2);
 
-$("#suma").attr('value',suma);
+    suma += " PLN";
+
+    $("#suma").attr('value', suma);
 
 });
-
--- app/assets/scripts/gallery.js
+++ app/assets/scripts/gallery.js
@@ -1,5 +1,12 @@
 var active_photo=1;
 var images_count = $(".thumb_button").length;
+var hr_width=0;
+document.addEventListener("mousemove",function(){
+    mouseMoved=true;
+    });
+    document.addEventListener("scroll",function(){
+        mouseMoved=true;
+        });
 document.addEventListener("click",function(e){
     if($(e.target).attr('class')=="thumb_button")
     {
@@ -15,18 +22,19 @@ document.addEventListener("click",function(e){
     {
         przewijaj(1);
     }
-    function przewijaj(a){
-        active_photo+=a;
 
-        if(active_photo>=images_count)
-        {
-            active_photo=0;
-        }
-        if(active_photo<0)
-        {
-            active_photo=images_count-1;
-        }
-        console.log(active_photo);
-        $("#obraz").attr("src",$(".thumb_button").eq(active_photo).attr('link'));
-    }
 });
+function przewijaj(a){
+    active_photo+=a;
+
+    if(active_photo>=images_count)
+    {
+        active_photo=0;
+    }
+    if(active_photo<0)
+    {
+        active_photo=images_count-1;
+    }
+    console.log(active_photo);
+    $("#obraz").attr("src",$(".thumb_button").eq(active_photo).attr('link'));
+}
